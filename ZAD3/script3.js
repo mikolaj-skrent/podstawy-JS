2320043b2db05a5565bc33fa30a0dbb96dd748b3
skrencikk <blablamiki4@gmail.com>
2026-04-28 10:35:11 +0200

    finished task 3

--- ZAD3/script3.js
+++ ZAD3/script3.js
@@ -1,20 +1,4 @@
-// $haslo = readline("Podaj swóje hasło: ");
-// $liczbaZnakow = strlen($haslo);
 
-// if ($liczbaZnakow < 5) {
-//     echo "Jesteś w kategorii bardzo słabe hasło!";
-
-
-// } else if ($liczbaZnakow >= 5 && $liczbaZnakow <= 8) {
-//     echo "Jesteś w kategorii słabe hasło!";
-
-// } else if ($liczbaZnakow >= 9 && $liczbaZnakow <= 11) {
-//     echo "Jesteś w kategorii silne hasło!";
-
-
-// } else if ($liczbaZnakow > 11) {
-//     echo "Jesteś w kategorii bardzo silne hasło!";
-// }
 const message = document.getElementById("message");
 const haslo = document.getElementById("haslo");
 
@@ -32,11 +16,12 @@ function checkPassword() {
     }
     if (haslo.value.length <= 6) {
         message.textContent = "Twoje hasło zalicza się do kategorii słabe.";
-        
+        message.style.color = "red";
         console.log("jestem 1 if");
 
     } else if (haslo.value.length > 6) {
         message.textContent = "Twoje hasło zalicza się do kategorii silne.";
+        message.style.color = "green";
         console.log("jestem 2 if");
     }
 }
