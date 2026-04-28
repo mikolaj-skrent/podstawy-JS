f8cbe9915b292c1a336936e7b25154c9bb2b9e31
skrencikk <blablamiki4@gmail.com>
2026-04-28 09:14:13 +0200

    finished task 2

---
+++ ZAD2/script2.js
@@ -0,0 +1,22 @@
+const bodyFontColor = document.getElementById('body')
+
+
+function defaultColor() {
+    bodyFontColor.style.color = "black";
+    console.log("Zmieniono kolor na domyślny!")
+}
+
+function red() {
+    bodyFontColor.style.color = "red";
+    console.log("Zmieniono kolor na czerwony!")
+}
+
+function green() {
+    bodyFontColor.style.color = "green";
+    console.log("Zmieniono kolor na zielony!")
+}
+
+function blue() {
+    bodyFontColor.style.color = "blue";
+    console.log("Zmieniono kolor na niebieski!")
+}
